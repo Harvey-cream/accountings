6293187601aea8ebd3eac2a604de61d3c5444ef3
lulongxiang <3328164031@qq.com>
2026-01-27 10:54:07 +0800

    项目构建

---
+++ pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="container">
+    <text>Redirecting...</text>
+  </view>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  uni.redirectTo({
+    url: '/pages/page_details/accounting_detail'
+  });
+});
+</script>
+
+<style>
+.container {
+  padding: 20px;
+  text-align: center;
+}
+</style>
