2ccb4bab4b316d35eba905086f22e39fe88c8a62
dragon_2C <3328164031@qq.com>
2026-03-06 01:53:56 +0800

    前端重构（需修改）

--- uni_accounting/vite.config.js
+++ uni_accounting/vite.config.js
@@ -1,8 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
 
+// 确保在 Vite 启动的最早阶段就设置好输入目录
+process.env.UNI_INPUT_DIR = path.resolve(__dirname, 'src');
+
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5174,
