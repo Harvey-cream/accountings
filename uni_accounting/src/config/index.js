1caa6b4ff3af685bcdd2341b0ca90124e63f8f56
lulongxiang <3328164031@qq.com>
2026-03-06 18:12:59 +0800

    https接口

--- uni_accounting/src/config/index.js
+++ uni_accounting/src/config/index.js
@@ -9,12 +9,11 @@ const IS_DEVELOPMENT = (typeof import.meta !== 'undefined' && import.meta.env &&
 // 统一 API 地址
 // 开发环境下连接本地局域网 IP，生产环境下连接云服务器 IP
 export const API_URL = (FORCE_DEV || IS_DEVELOPMENT)
-	// ? "http://192.168.146.1:8030" 
 	?
 	"http://192.168.110.134:8030" :
-	"http://47.107.238.136:8011"
-
+	"https://draccounting.xin"
+	// "http://47.107.238.136:8011"
 // 如果未来需要 WebSocket，也可以在这里预留
 export const WS_URL = (FORCE_DEV || IS_DEVELOPMENT) ?
 	"ws://192.168.110.134:8030" :
-	"ws://47.107.238.136:8011"
+	"wss://draccounting.xin"
